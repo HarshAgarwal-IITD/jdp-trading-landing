ace1a17a1025993a3ec13855a3c4d23e8535eeb0
Harsh Agarwal <harshagarwal91016@gmail.com>
2026-01-04 14:28:51 +0530

    Add 'Settings' link to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,6 +11,7 @@ const Navbar = () => {
     { name: "About", href: "#about" },
     { name: "Clients", href: "#clients" },
     { name: "Contact", href: "#contact" },
+    { name: "Settings" , href: "#Settings"}
   ];
 
   return (
